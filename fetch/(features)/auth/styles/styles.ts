7f1e78079895ddd1090d8b4fa55f202245992750
Alejandro Tapia <tapia6176@gmail.com>
2025-03-10 15:08:51 -0700

    Modal Updates, to make finding a match feel more magical!

--- fetch/(features)/auth/styles/styles.ts
+++ fetch/(features)/auth/styles/styles.ts
@@ -1,5 +1,5 @@
-export const buttonBase = "rounded-md border p-2 hover:cursor-pointer";
-export const inputBase = "w-full rounded-md border p-2 focus:ring-2";
+export const buttonBase = "rounded-md p-2 hover:cursor-pointer";
+export const inputBase = "w-full rounded-md border border-gray-400 p-2 focus:ring-2";
 export const textBase = "text-gray-500 text-sm text-center";
 export const containerBase = "overflow-hidden rounded-lg border shadow-lg bg-white";
 export const flexColumn = "flex flex-col gap-6";
--- fetch/(features)/filters/styles/styles.ts
+++ fetch/(features)/filters/styles/styles.ts
@@ -1,2 +1,2 @@
-export const inputBase = "rounded-lg border border-gray-300 p-2 mb-1 cursor-pointer";
+export const inputBase = "w-fit bg-white rounded-lg border border-gray-300 p-2 mb-1 cursor-pointer hover:bg-gray-100";
 
